a44011c88b01972e413f4ec8a44e662eece4ba34
-=NeXuS=- <schwartzmann.a@gmail.com>
2019-07-01 15:59:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ api = Api(app)
 bouchon = True
 awx_url = os.environ['AWX_URL']
 #awx_url = 'http://127.0.0.1:5002'
-awx_token = '20wXXDKw4O0q3Z5TNX6BL9IuRN4HbI' # local awx admin test token
+awx_token = 'Vj4L9wce9yLBwFl3LuDzUm9NKnCN0R' # local awx admin test token
 
 class MyForm(FlaskForm):
    name = StringField('Nom', validators=[validators.DataRequired()])
